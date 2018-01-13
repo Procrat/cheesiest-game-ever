cc7774c52a4df1ed9d37c449591c59a4dc9869ec
Stijn Seghers <stijnseghers@gmail.com>
2018-01-13 16:28:02 +1300

    Animate ALL THE DIRECTIONS

--- game/player/player.gd
+++ game/player/player.gd
@@ -10,6 +10,10 @@ var right_action
 var up_action
 var down_action
 
+onready var walking_animations = get_node("walking")
+
+onready var animation
+
 func _ready():
 	set_fixed_process(true)
 	var player_name_str = "myrjam" if player_name == MYRJAM else "stijn"
@@ -20,18 +24,39 @@ func _ready():
 
 func _fixed_process(delta):
 	var direction = Vector2()
-	if Input.is_action_pressed(left_action):
-		direction.x -= 1
-	if Input.is_action_pressed(right_action):
-		direction.x += 1
+	var animation = null
+	var flipped = false
 	if Input.is_action_pressed(up_action):
 		direction.y -= 1
+		animation = walking_animations.get_node("back")
 	if Input.is_action_pressed(down_action):
 		direction.y += 1
+		animation = walking_animations.get_node("front")
+	if Input.is_action_pressed(left_action):
+		direction.x -= 1
+		animation = walking_animations.get_node("right")
+		flipped = true
+	if Input.is_action_pressed(right_action):
+		direction.x += 1
+		animation = walking_animations.get_node("right")
 	
+	if animation == null:
+		return
+	
+	move_in_direction(direction, delta)
+	set_animation(animation, flipped)
+
+func move_in_direction(direction, delta):
 	var motion = move(direction * walk_speed * delta)
 	
 	if (is_colliding()):
 		var normal = get_collision_normal()
 		motion = normal.slide(motion)
 		move(motion)
+
+func set_animation(animation, flipped):
+	if self.animation != null:
+		self.animation.set_hidden(true)
+	animation.set_hidden(false)
+	animation.flip_h = flipped
+	self.animation = animation
