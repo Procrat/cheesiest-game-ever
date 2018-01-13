c76018d82a6fdc457c761b30cdd0771c1a2e728b
Stijn Seghers <stijnseghers@gmail.com>
2018-01-13 17:42:18 +1300

    Better way of scene switching

--- game/scenes/next_scene.gd
+++ game/scenes/next_scene.gd
@@ -3,8 +3,9 @@ extends Node
 export(String, FILE, "*.tscn") var next_scene
 
 func _ready():
-	set_process(true)
+	set_process_input(true)
 
-func _process(delta):
-	if Input.is_action_pressed("ui_accept"):
+func _input(event):
+	if event.is_action_pressed("ui_accept"):
 		get_tree().change_scene(next_scene)
+	get_tree().set_input_as_handled()
