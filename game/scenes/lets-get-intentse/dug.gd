d7184dc31598a6eb9f21a64d74368d3ef5b4ce23
Stijn Seghers <stijnseghers@gmail.com>
2018-01-28 14:52:33 +1300

    You shall not pass!

--- game/scenes/lets-get-intentse/dug.gd
+++ game/scenes/lets-get-intentse/dug.gd
@@ -6,18 +6,16 @@ export(float) var speed
 var initialised = false
 var home_base  # Global pos
 var followee  # Global pos
-var level
-var navigation
-var animations
+
+onready var level = get_node("/root/level")
+onready var navigation = level.get_node("navigation")
+onready var animations = get_node("animations")
 
 
 func _ready():
 	if not initialised:
 		set_fixed_process(true)
 		home_base = get_global_pos()
-		level = get_node("/root/level")
-		navigation = level.get_node("navigation")
-		animations = get_node("animations")
 		for dug_attractor in get_tree().get_nodes_in_group("dug-attractors"):
 			dug_attractor.connect("body_enter", self, "stick_encountered_something", [dug_attractor])
 
@@ -31,23 +29,36 @@ func _fixed_process(delta):
 	var is_following = followee != null and followee.picked_up
 	if not is_following:
 		followee = null
-	var goal = followee.get_global_pos() if is_following else home_base
-	var path = navigation.get_simple_path(pos_relative_to(self.get_global_pos(), level), pos_relative_to(goal, level))
+	
+	var goal
+	if is_following:
+		var enemy_path = navigation.get_simple_path(pos_relative_to(followee.get_global_pos(), level), pos_relative_to(home_base, level))
+		goal = find_defending_position(enemy_path, 100)
+	else:
+		goal = home_base
+	
+	var path = navigation.get_simple_path(pos_relative_to(get_global_pos(), level), pos_relative_to(goal, level))
 	path.remove(0)
 	if path.size() <= 0:
 		return
-	if not is_following or self.get_global_pos().distance_to(goal) > min_distance_to_stick:
-		move_and_slide((path[0] - get_global_pos()).clamped(speed * delta))
-	else:
-		idle()
+	
+	move_and_slide((path[0] - get_global_pos()).clamped(speed * delta))
+
+
+func find_defending_position(path, distance_from_start):
+	var distance_left = distance_from_start
+	for i in range(path.size() - 1):
+		var path_diff = path[i+1] - path[i]
+		var part_length = path_diff.length()
+		if part_length > distance_left:
+			return path[i] + path_diff.clamped(distance_left)
+		else:
+			distance_left -= part_length
+	return path[path.size() - 1]
 
 
 func pos_relative_to(target_global_pos, relative_to):
 	return relative_to.get_global_transform().xform_inv(target_global_pos)
-
-
-func idle():
-	animations.play("idle")
 
 
 func move_and_slide(relative_translation):
@@ -61,8 +72,7 @@ func move_and_slide(relative_translation):
 		move(motion)
 	
 	if (get_pos() - old_pos).length() < 1:
-		idle()
-		return
-	
-	animations.flip_h = relative_translation.x < 0
-	animations.play("running middle")
+		animations.play("idle")
+	else:
+		animations.flip_h = relative_translation.x < 0
+		animations.play("running middle")
