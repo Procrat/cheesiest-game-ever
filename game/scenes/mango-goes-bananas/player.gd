80854156d42886b82768073de33e88eb1d81bb63
Stijn Seghers <stijnseghers@gmail.com>
2018-02-05 05:58:16 +1300

    Oh god, I don't want to study again D:

--- game/scenes/mango-goes-bananas/player.gd
+++ game/scenes/mango-goes-bananas/player.gd
@@ -2,10 +2,12 @@ extends "res://game/player/player.gd"
 
 var utils = preload("res://game/utils.gd")
 
-onready var sofa = get_parent().get_node("sofa-location")
+onready var sofa_myrjam = get_parent().get_node("sofa-myrjam-location")
+onready var sofa_stijn = get_parent().get_node("sofa-stijn-location")
 
 var do_action
 var drawing = false
+var studying = false
 
 
 func _ready():
@@ -16,11 +18,18 @@ func _ready():
 
 func _input(event):
 	if event.is_action_released(do_action):
-		if player_name == MYRJAM and is_sofa_nearby():
-			if not drawing:
-				draw()
-			else:
-				stop_drawing()
+		if player_name == MYRJAM:
+			if is_sofa_myrjam_nearby():
+				if not drawing:
+					draw()
+				else:
+					stop_drawing()
+		elif player_name == STIJN:
+			if is_sofa_stijn_nearby():
+				if not studying:
+					study()
+				else:
+					stop_studying()
 
 
 func fixed_process(delta):
@@ -28,14 +37,14 @@ func fixed_process(delta):
 		.fixed_process(delta)
 
 
-func is_sofa_nearby():
-	print(get_global_pos().distance_to(sofa.get_global_pos()))
-	return get_global_pos().distance_to(sofa.get_global_pos()) < 100
+func is_sofa_myrjam_nearby():
+	return get_global_pos().distance_to(sofa_myrjam.get_global_pos()) < 30
 
 
 func draw():
-	print("start drawing")
+	animations.set_flip_h(false)
 	animations.play("draw start")
+	set_global_pos(sofa_myrjam.get_global_pos())
 	drawing = true
 	utils.do_once_after_animation(animations, self, "keep_drawing")
 
@@ -49,9 +58,31 @@ func stop_drawing():
 	utils.do_once_after_animation(animations, self, "unbusy")
 
 
+func is_sofa_stijn_nearby():
+	return get_global_pos().distance_to(sofa_stijn.get_global_pos()) < 30
+
+
+func study():
+	animations.set_flip_h(false)
+	animations.play("study start")
+	set_global_pos(sofa_stijn.get_global_pos())
+	studying = true
+	utils.do_once_after_animation(animations, self, "keep_studying")
+
+
+func keep_studying():
+	animations.play("study middle")
+
+
+func stop_studying():
+	animations.play("study end")
+	utils.do_once_after_animation(animations, self, "unbusy")
+
+
 func is_busy():
-	return drawing
+	return drawing or studying
 
 
 func unbusy():
 	drawing = false
+	studying = false
