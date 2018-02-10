c730dcd0f7eff27e6e1965c1d58be7f906cb7eae
Stijn Seghers <stijnseghers@gmail.com>
2018-02-10 15:00:25 +1300

    Single player instructions

--- game/scenes/level-select/level-select.gd
+++ game/scenes/level-select/level-select.gd
@@ -2,16 +2,21 @@ extends Node2D
 
 export(String, FILE, "*.tscn") var lvl1
 export(PackedScene) var instructions_lvl1
+export(PackedScene) var instructions_lvl1_single
 export(String, FILE, "*.tscn") var lvl2
 export(PackedScene) var instructions_lvl2
+export(PackedScene) var instructions_lvl2_single
 export(String, FILE, "*.tscn") var lvl3
 export(PackedScene) var instructions_lvl3
+export(PackedScene) var instructions_lvl3_single
 export(String, FILE, "*.tscn") var lvl4
 export(PackedScene) var instructions_lvl4
+export(PackedScene) var instructions_lvl4_single
 
 
 onready var buttons = get_node("buttons").get_children()
-onready var instructions_scenes = [instructions_lvl1, instructions_lvl2, instructions_lvl3, instructions_lvl4]
+onready var instructions_scenes_multiplayer = [instructions_lvl1, instructions_lvl2, instructions_lvl3, instructions_lvl4]
+onready var instructions_scenes_singleplayer = [instructions_lvl1_single, instructions_lvl2_single, instructions_lvl3_single, instructions_lvl4_single]
 onready var level_names = [lvl1, lvl2, lvl3, lvl4]
 
 var instructions_scene
@@ -32,10 +37,15 @@ func show_instructions(level_idx):
 		if instructions_idx == level_idx:
 			return
 		remove_child(instructions_scene)
-	var instructions = instructions_scenes[level_idx].instance()
-	add_child(instructions)
-	instructions.get_node("play-button").connect("pressed", self, "start_level", [level_idx])
-	instructions_scene = instructions
+		
+	if GLOBAL_STATE.is_single_player:
+		instructions_scene = instructions_scenes_singleplayer[level_idx].instance()
+	else:
+		instructions_scene = instructions_scenes_multiplayer[level_idx].instance()
+	
+	add_child(instructions_scene)
+	instructions_scene.get_node("play-button").connect("pressed", self, "start_level", [level_idx])
+	
 	instructions_idx = level_idx
 
 
