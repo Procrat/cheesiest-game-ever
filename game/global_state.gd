666438e5a733905cfa2e5cec46069247519a8cfb
Stijn Seghers <stijnseghers@gmail.com>
2018-02-11 08:33:50 +1300

    Remove stuff

--- game/global_state.gd
+++
@@ -1,8 +0,0 @@
-extends Node
-
-var highest_level_unlocked = 0
-var is_single_player = false
-
-func ensure_level_unlocked(level):
-	if level > highest_level_unlocked:
-		highest_level_unlocked = level
